f978474dd446e522bf59328e3bb057423722b6e4
rafaellaevboff <rafaella-eboff@rede.ulbra.br>
2022-07-06 15:18:09 -0300

    revitalidade e controle de estado

---
+++ 2-ComponentesMetodosCicloDeVida/vue-parte2.2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <button @click="count.x++, count.a++">Click</button>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: {
+        x: 0,
+      }
+    }
+  },
+  mounted() {
+    console.log(this);
+    this.$set(this.count, 'y', 1)
+    this.count = {...this.count, ...{ a:1, b:2}}
+  }
+}
+</script>
+
+<style>
+  #app {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100vh;
+  }
+</style>
